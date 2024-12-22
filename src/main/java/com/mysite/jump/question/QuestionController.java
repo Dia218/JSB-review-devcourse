2f371b568cde7012dc6ba276fbd1ddb9d22ff905
TTaiJin <taeparis980110@gmail.com>
2024-12-23 01:00:42 +0900

    [BE4] 윤태진 - jsb

---
+++ src/main/java/com/mysite/jump/question/QuestionController.java
@@ -0,0 +1,119 @@
+package com.mysite.jump.question;
+
+import com.mysite.jump.answer.Answer;
+import com.mysite.jump.answer.AnswerForm;
+import com.mysite.jump.answer.AnswerService;
+import com.mysite.jump.user.SiteUser;
+import com.mysite.jump.user.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+
+@RequestMapping("/question")
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+    private final UserService userService;
+
+    @GetMapping("/list")
+    public String list(Model model,
+                       @RequestParam(value = "page", defaultValue = "0") int page,
+                       @RequestParam(value = "kw", defaultValue = "") String kw,
+                       @RequestParam(value = "sortOption", defaultValue = "newest") String sortOption) {
+        Page<Question> paging = this.questionService.getList(page, kw, sortOption);
+        model.addAttribute("paging", paging);
+        model.addAttribute("kw", kw);
+        model.addAttribute("sortOption", sortOption);
+        return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(Model model,
+                         @PathVariable("id") Integer id,
+                         AnswerForm answerForm,
+                         @RequestParam(value = "page", defaultValue = "0") int page,
+                         @RequestParam(value = "sortOption", defaultValue = "newest") String sortOption) {
+        Question question = this.questionService.getQuestion(id);
+
+        Page<Answer> paging = this.answerService.getList(question, page, 20, sortOption);
+        model.addAttribute("question", question);
+        model.addAttribute("paging", paging);
+        model.addAttribute("sortOption", sortOption);
+        return "question_detail";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
+        return "redirect:/question/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+        return "question_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm, @PathVariable("id") Integer id, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/detail/" + id;
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/vote/{id}")
+    public String questionVote(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        this.questionService.vote(question, siteUser);
+        return "redirect:/question/detail/" + id;
+    }
+}
